e9eb8388cdca3b95715ce23aa9b96911aca33995
Elissa <elissadusabe@gmail.com>
2023-05-26 21:42:11 +0200

    new features

---
+++ src/components/thirdDiv.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import './styles/thirdDiv.css'
+import Impact from './styles/impact.png'
+
+function thirdDiv() {
+  return (
+    <div className='mainT'>
+        <div className="connectD">
+            <div className="textT">
+              <h2>Connect with your scientific community</h2>
+              <p>Share your research, collaborate with your peers, and get the support you need to advance your career.</p>
+            </div>
+ 
