a3039b4d72b67bbb451759cca008f5f5df74e9f4
Elissa <elissadusabe@gmail.com>
2023-05-26 21:42:56 +0200

    new features

--- src/components/thirdDiv.jsx
+++ src/components/thirdDiv.jsx
@@ -11,4 +11,28 @@ function thirdDiv() {
               <h2>Connect with your scientific community</h2>
               <p>Share your research, collaborate with your peers, and get the support you need to advance your career.</p>
             </div>
- 
+            <div className="visit">
+                <h4>VISIT TOPIC PAGES</h4>
+                <button>Engineering</button>
+                <button>Mathematics</button>
+                <br />
+                <button>Bilogy</button>
+                <button>Computer Science</button>
+                <br />
+                <button>Climate Change</button>
+                <button>Medicine</button>
+                <br />
+                <button>Physics</button>
+                <button>Social Science</button>
+                <br />
+                <button>Astrophysics</button>
+                <button>Chemistry</button>
+            </div>
+            <br />
+            <div className="impactD">
+                <div>
+                   <img src={Impact} className='impact'/>
+                </div>
+                <div className="textI">
+                    <h2>Measure your impact</h2>
+                    <br />
