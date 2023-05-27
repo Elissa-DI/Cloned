88d66fd24263fb692dc6b7443ac995c1311e2d1c
Elissa <elissadusabe@gmail.com>
2023-05-27 23:43:16 +0200

    new features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/Nav';
+import FirstDiv from './components/firstDiv';
+import SecondDiv from './components/secondDiv';
+import ThirdDiv from './components/thirdDiv';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
-  );
-}
-
-export default App;
+      < Nav/>
+      <FirstDiv />
